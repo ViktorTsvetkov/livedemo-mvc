c2e440f433538d4a1c3cbae13d42a41659808802
ENTERPRISE\telerikacademy <telerikacademytrainers@gmail.com>
2017-03-07 17:24:23 +0200

    Added integration tests!

---
+++ LiveDemo-MVC/LiveDemo-MVC.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LiveDemo_MVC.Models;
+
+namespace LiveDemo_MVC.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            // Act
+            int usersCount = context.Users.Count();
+
+            // Assert
+            Assert.AreEqual(1, usersCount);
+        }
+    }
+}
