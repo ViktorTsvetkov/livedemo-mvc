72f381c5967972f3b7404b4ca87fff10ed2c367d
ENTERPRISE\telerikacademy <telerikacademytrainers@gmail.com>
2017-03-23 12:36:54 +0200

    added unit tests for BookService

---
+++ LiveDemo-MVC/LiveDemo-MVC.UnitTests/LiveDemo_MVC.DataServices/BookServiceTests/Constructor_Should.cs
@@ -0,0 +1,51 @@
+﻿using LiveDemo_MVC.Data.Contracts;
+using LiveDemo_MVC.Data.Models;
+using LiveDemo_MVC.DataServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveDemo_MVC.UnitTests.LiveDemo_MVC.DataServices.BookServiceTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void ReturnsAnInstance_WhenBothParametersAreNotNull()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Book>>();
+            var dbContextMock = new Mock<ILiveDemoEfDbContextSaveChanges>();
+
+            // Act
+            BookService bookService = new BookService(wrapperMock.Object, dbContextMock.Object);
+
+            // Assert
+            Assert.IsNotNull(bookService);
+        }
+
+        [TestMethod]
+        public void ThrowException_WhenBookSetWrapperIsNull()
+        {
+            // Arrange
+            var dbContextMock = new Mock<ILiveDemoEfDbContextSaveChanges>();
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new BookService(null, dbContextMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowException_WhenDbContextIsNull()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Book>>();
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new BookService(wrapperMock.Object, null));
+        }
+    }
+}
---
+++ LiveDemo-MVC/LiveDemo-MVC.UnitTests/LiveDemo_MVC.DataServices/BookServiceTests/GetById_Should.cs
@@ -0,0 +1,72 @@
+﻿using LiveDemo_MVC.Data.Contracts;
+using LiveDemo_MVC.Data.Models;
+using LiveDemo_MVC.DataServices;
+using LiveDemo_MVC.DataServices.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveDemo_MVC.UnitTests.LiveDemo_MVC.DataServices.BookServiceTests
+{
+    [TestClass]
+    public class GetById_Should
+    {
+        [TestMethod]
+        public void ReturnModel_WhenThereIsAModelWithThePassedId()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Book>>();
+            var dbContextMock = new Mock<ILiveDemoEfDbContextSaveChanges>();
+            Guid? bookId = Guid.NewGuid();
+
+            wrapperMock.Setup(m => m.GetById(bookId.Value)).Returns(new Book() { Id = bookId.Value });
+
+            BookService bookService = new BookService(wrapperMock.Object, dbContextMock.Object);
+
+            // Act
+            BookModel bookModel = bookService.GetById(bookId);
+
+            // Assert
+            Assert.IsNotNull(bookModel);
+        }
+
+        [TestMethod]
+        public void ReturnNull_WhenIdIsNull()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Book>>();
+            var dbContextMock = new Mock<ILiveDemoEfDbContextSaveChanges>();
+            
+            BookService bookService = new BookService(wrapperMock.Object, dbContextMock.Object);
+
+            // Act
+            BookModel bookModel = bookService.GetById(null);
+
+            // Assert
+            Assert.IsNull(bookModel);
+        }
+
+        [TestMethod]
+        public void ReturnNull_WhenThereIsNoModelWithThePassedId()
+        {
+            // Arrange
+            var wrapperMock = new Mock<IEfDbSetWrapper<Book>>();
+            var dbContextMock = new Mock<ILiveDemoEfDbContextSaveChanges>();
+            Guid? bookId = Guid.NewGuid();
+
+            wrapperMock.Setup(m => m.GetById(bookId.Value)).Returns((Book)null);
+
+            BookService bookService = new BookService(wrapperMock.Object, dbContextMock.Object);
+
+            // Act
+            BookModel bookModel = bookService.GetById(bookId);
+
+            // Assert
+            Assert.IsNull(bookModel);
+        }
+    }
+}
